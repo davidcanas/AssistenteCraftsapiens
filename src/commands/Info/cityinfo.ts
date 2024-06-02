1c39556a1a4c82cbfe2290ccd7534a20cba6a68d
David <devcanas21@gmail.com>
2024-06-02 19:03:57 +0100

    Update cityinfo.ts

--- src/commands/Info/cityinfo.ts
+++ src/commands/Info/cityinfo.ts
@@ -31,7 +31,7 @@ export default class cityinfo extends Command {
 				ctx.sendMessage('Você precisa especificar um jogador!');
 				return;
 			}
-			const req = await this.client.fetch('http://jogar.craftsapiens.com.br:10004/up/world/Earth/').then(a => a.json());
+			const req = await this.client.fetch('http://jogar.craftsapiens.com.br:2053/up/world/Earth/').then(a => a.json());
 			const cityinfo = this.client.utils.dynmap.findCityInfo(req, ctx.args[0]);
 			const mayorinfo = await this.client.getPlayerInfo(cityinfo?.mayor);
 			let habitantes_n = '1';
@@ -66,7 +66,7 @@ export default class cityinfo extends Command {
 		}
 
 		const allCities = await this.client.utils.dynmap.getAllRegisteredCities(
-			await this.client.fetch('http://jogar.craftsapiens.com.br:10004/up/world/Earth/').then(a => a.json())
+			await this.client.fetch('http://jogar.craftsapiens.com.br:2053/up/world/Earth/').then(a => a.json())
 		);
 
 
