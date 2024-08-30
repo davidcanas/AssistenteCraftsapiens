b5db0aa998f58909fb6fee5139cfb606a1d732b6
davidcanas <canasdev@proton.me>
2024-08-30 18:30:53 +0100

    ✨ | v5.0, Big update, add Nina,Luy and Ada and many other things

--- src/web/app.ts
+++ src/web/app.ts
@@ -81,7 +81,7 @@ app.get("/stats/survival", async (req, res) => {
 	res.status(200).render("stats_survival", { 
     user: req.user,
     member: client.guilds.get("892472046729179136").members.get(req.user?.id),
-    avatar: client.users.get(req.user.id).avatarURL(),
+    avatar: client.users.get(req.user?.id)?.avatarURL(),
     guild: client.guilds.get("892472046729179136"),
     highestRole: client.getHighestRole,
     playerList,
