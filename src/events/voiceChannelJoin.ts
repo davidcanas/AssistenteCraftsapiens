a5a5d8161c0bfb9930e7ed29d8f9c14d9e31f4f0
davidcanas <canasdev@proton.me>
2024-09-12 21:04:28 +0100

    fix: Fix bug on counting call hours;

--- src/events/voiceChannelJoin.ts
+++ src/events/voiceChannelJoin.ts
@@ -14,14 +14,14 @@ export default class voiceChannelJoin {
         const studyChannels = ["1005889304658202704", "1005889381762084976", "1005889464293392466", "1272703689567768598"];
 
         if (!studyChannels.includes(channel.id)) return;
-        
-        if(member.bot) return;
-        
+
+        if (member.bot) return;
+
         const user = await this.client.db.users.findOne({ id: member.id });
-      
+
         if (!user) {
             await this.client.db.users.create({
-                _id: new ObjectId(), 
+                _id: new ObjectId(),
                 id: member.id,
                 nick: member.nick || null,
                 voiceSessions: []
@@ -29,15 +29,20 @@ export default class voiceChannelJoin {
         }
 
         const joinTime = new Date();
-        
+
         console.log(`(${member.username} - entrou na Call de Estudo às ${joinTime}`);
-      
+
+        if (user && user.voiceSessions.find((s: any) => s.channel === channel.id && s.leaveTime === null)) {
+            console.log(`(${member.username} - já está na Call de Estudo, não foi possível adicionar uma nova sessão, evitando possivel bug :D :sunglasses:, eu me amo.`);
+            return;
+        }
+
         await this.client.db.users.updateOne(
             { id: member.id },
             {
                 $push: {
                     voiceSessions: {
-                        _id: new ObjectId(), 
+                        _id: new ObjectId(),
                         channel: channel.id,
                         joinTime: joinTime,
                         leaveTime: null,
