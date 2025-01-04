dbb55007cb3bfac853da7571456ef1f3702bdbbd
davidcanas <devcanas21@gmail.com>
2025-01-04 23:24:56 +0000

    Update - fix ask command

--- src/commands/Util/askgpt.ts
+++ src/commands/Util/askgpt.ts
@@ -25,11 +25,6 @@ export default class askGPT extends Command {
     }
 
     async execute(ctx: CommandContext): Promise<void> {
-
-        if (ctx.type === 1) {
-            ctx.sendMessage({content: "Este comando foi desativado e provavelmente será deletado em breve\nPedimos desculpa pelo incómodo.", flags: 1 << 6});
-            return;
-        } else return;
         
         await ctx.defer();
 
@@ -51,18 +46,18 @@ export default class askGPT extends Command {
 
         const headers = {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.GPT_KEY}`
+            "Authorization": `${process.env.GEMINI_KEY}`
         };
 
-        const systemMessagesPath = path.resolve(__dirname, "../../data/system_context.txt");
-        const systemMessages = fs.readFileSync(systemMessagesPath, "utf-8").split("\n").filter(line => line.trim());
+        const userMessagesPath = path.resolve(__dirname, "../../data/user_context.txt");
+        const userMessages = fs.readFileSync(userMessagesPath, "utf-8").split("\n").filter(line => line.trim());
 
         const townyDocsPath = path.resolve(__dirname, "../../data/towny_docs.txt");
         const townyDocsMessages = fs.readFileSync(townyDocsPath, "utf-8").split("\n").filter(line => line.trim());
 
         const timestamp = new Date().toISOString();
-        const messages = systemMessages.map(content => ({
-            role: "system",
+        const messages = userMessages.map(content => ({
+            role: "user",
             content: content
                 .replace("{member_name}", ctx.member.nick || ctx.member.user.globalName)
                 .replace("{member_role}", this.client.getHighestRole(ctx.guild, ctx.member.id))
@@ -72,9 +67,9 @@ export default class askGPT extends Command {
                 .replace("{useful_links}", usefulLinks)
                 .replace("{timestamp}", timestamp)
         }));
-        messages.push({role: "system", content: `Atualmente o Survival Geopolítico tem ${this.client.cache.towns.length} cidades ativas e ${this.client.cache.towns.filter(a => a.ruined).length} cidades em ruinas (no mundo geopolitico) `});
-        messages.push({ role: "system", content: townyDocsMessages.join("\n") });
-        messages.push({ role: "user", content: ctx.args.join(" ") });
+        messages.push({role: "user", content: `Atualmente o Survival Geopolítico tem ${this.client.cache.towns.length} cidades ativas e ${this.client.cache.towns.filter(a => a.ruined).length} cidades em ruinas (no mundo geopolitico) `});
+        messages.push({ role: "user", content: townyDocsMessages.join("\n") });
+        messages.push({ role: "user", content: `\nMensagem a responder: "${ctx.args.join(" ")}"`});
 
         const inputText = ctx.args.join(" ");
 
@@ -110,42 +105,40 @@ export default class askGPT extends Command {
         if (detectedCities.length > 0) {
             
             const townInfo = detectedCities.map(town => `${town.name}:\nPrefeito: ${town.mayor}\nCoordenadas: X: ${town.coords.x} Z: ${town.coords.z}\nNação: ${town.nation}\nHabitantes: ${town.members.length} (${town.members.join(", ")})\nEm ruínas: ${town.ruined}\n-`).join("\n");
-            messages.push({ role: "system", content: `Cidades mencionadas pelo jogador (ao calcular distâncias, omita os cálculos, diga diretamente o resultado):\n${townInfo}` });
+            messages.push({ role: "user", content: `Cidades mencionadas pelo jogador (ao calcular distâncias, omita os cálculos, diga diretamente o resultado):\n${townInfo}` });
         }
 
         if (mentionedResidents.length > 0) {
             const residentInfo = mentionedResidents.map(({ resident, town }) => `${resident} é residente de ${town.name}:\nPrefeito: ${town.mayor}\nCoordenadas: X: ${town.coords.x} Z: ${town.coords.z}\nNação: ${town.nation}\nHabitantes: ${town.members.length} (${town.members.join(", ")})\nEm ruínas: ${town.ruined}\n-`).join("\n");
-            messages.push({ role: "system", content: `Jogadores (habitantes) mencionados pelo jogador:\n${residentInfo}` });
+            messages.push({ role: "user", content: `Jogadores (habitantes) mencionados pelo jogador:\n${residentInfo}` });
         }
 
         const data = {
-            "model": "gpt-4o",
-            "messages": messages,
-            "max_tokens": 700,
-            "temperature": 0.57
+            "model": "gemini-2.0-flash-exp",
+            "contents": messages
         };
 
-        const response = await fetch(process.env.GPT_URL, {
+        const response = await fetch(process.env.GEMINI_URL, {
             method: "POST",
             headers: headers,
             body: JSON.stringify(data)
         });
 
         const json = await response.json();
-        if (json.error) {
+        if (!json.candidates) {
             ctx.sendMessage(`Olá, ${ctx.member.nick || ctx.member.user.globalName}, ocorreu um erro ao tentar processar a sua pergunta. Provavelmente é algum problema com a OpenIA. Tente novamente mais tarde!`);
             console.log(json.error.message)
             return;
         }
 
-        if (json.choices[0].message.content.includes("timeout_member")) {
+        if (json.candidates[0].content.parts[0].text.includes("timeout_member")) {
             ctx.member.edit({ communicationDisabledUntil: new Date(Date.now() + 3600000).toISOString() });
-            json.choices[0].message.content = json.choices[0].message.content.replace("[timeout_member]", " ");
+            json.candidates[0].content.parts[0].text = json.candidates[0].content.parts[0].text.replace("[timeout_member]", " ");
         }
 
         const embed = new this.client.embed()
             .setColor("RANDOM")
-            .setDescription(json.choices[0].message.content);
+            .setDescription(json.candidates[0].content.parts[0].text);
         ctx.sendMessage({ embeds: [embed] });
     }
 }
