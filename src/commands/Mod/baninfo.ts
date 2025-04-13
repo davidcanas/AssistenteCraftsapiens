d5d95c624e0a58443ea62260b9ece2bbb70b6db9
davidcanas <devcanas21@gmail.com>
2025-04-13 23:44:07 +0100

    💫 | Small changes

--- src/commands/Mod/baninfo.ts
+++ src/commands/Mod/baninfo.ts
@@ -35,7 +35,7 @@ export default class banInfo extends Command {
 
         if (!banInfo) {
             ctx.sendMessage({
-                content: `<:mine_no:939943857754365962> O usuário ${user?.tag} **não está** banido do servidor!`,
+                content: `<:mine_no:939943857754365962> O usuário ${user?.username} **não está** banido do servidor!`,
                 flags: 1 << 6,
             });
             return;
@@ -44,7 +44,7 @@ export default class banInfo extends Command {
         const embed = new this.client.embed()
             .setTitle("<:report:1307789599279546419> Informações de Banimento")
             .setDescription(
-                `<:Steve_chorando:905024572036907008> O membro ${user.tag} está banido do servidor.\n\n<:text:1308134831946862732> **Motivo:**\n \`${banInfo.reason || "Sem motivo especificado"}\``
+                `<:Steve_chorando:905024572036907008> O membro ${user.username} está banido do servidor.\n\n<:text:1308134831946862732> **Motivo:**\n \`${banInfo.reason || "Sem motivo especificado"}\``
             )
             .setThumbnail(user.avatarURL())
     
